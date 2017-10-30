4a39ae98e7e5c538a1c857f52ccdebcc152c6186
Jennifer Schommer <jenniferschommer@invenshure.com>
2017-10-30 12:59:00 -0500

    Fix commas in is_functional

--- measurement_from_label.py
+++ measurement_from_label.py
@@ -102,10 +102,10 @@ def is_functional(label):
         re.compile('wedges', re.IGNORECASE),
         re.compile('emoreg', re.IGNORECASE),
         re.compile('conscious', re.IGNORECASE),
-        re.compile('^REST$')
+        re.compile('^REST$'),
         re.compile('ep2d', re.IGNORECASE),
         re.compile('_task_', re.IGNORECASE),
-        re.compile('_rest_', re.IGNORECASE),
+        re.compile('_rest_', re.IGNORECASE)
         ]
     return regex_search_label(regexes, label)
 
